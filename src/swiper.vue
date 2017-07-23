a3ba811604b7f733581a25407e0592acc581959f
黄明 <huangming@huangmingdeMBP.lan>
2017-07-23 16:44:34 +0800

    feat: init

---
+++ src/swiper.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="ro-swiper-container">
+		<Touch
+			@touch-start="onTouchStart">
+			<div v-for="image in images">
+				<img :src="image">
+			</div>
+		</Touch>
+	</div>
+</template>
+
+<script>
+	import Touch from '@forzoom/touch';
+	export default {
+		name: 'Swiper',
+		props: {
+			/**
+			 * 每张图片的展示时间
+			 */
+			duration: {
+				type: Number,
+				default: 3000,
+			},
+			/**
+			 * 所有需要展示的图片
+			 */
+			images: {
+				required: true,
+				type: Array,
+			},
+		},
+		methods: {
+			onTouchStart() {
+				console.log('onTouchStart');
+			},
+		},
+	};
+</script>
+
+<style>
+	
+</style>
